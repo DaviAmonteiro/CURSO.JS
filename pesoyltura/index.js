04796163d60ba1a2bd96bfcf4f3e6ceb64a745ef
DaviAmonteiro <davialexandremonteiro0@gmail.com>
2025-12-11 16:55:00 -0300

    Corrige comentário
    Comentário revisado para melhorar a compreensão do código.

--- pesoyltura/index.js
+++ pesoyltura/index.js
@@ -5,7 +5,7 @@ function calcularIMC() {
   var p = Number(peso.value);
   var a = Number(altura.value);
  if (a>10){
-    a=a/100  //converte em cm! 
+    a=a/100  
  }
 var img= window.document.createElement('img')
 img.setAttribute('id', 'foto')
@@ -17,7 +17,7 @@ img.setAttribute('id', 'foto')
   res.innerHTML= `<strong>Seu peso corporal é ${calculo.toFixed(2)}</strong>`
 
 if (calculo<18.5){
-    //magreza
+    //magro
     img.setAttribute('src', 'magro.jpg')
 }
 else if(calculo>=18.5 && calculo <= 24.9){
@@ -37,6 +37,4 @@ res.appendChild(img)
 }
 
 
-/*O cálculo do IMC é feito dividindo o peso (em quilogramas) pela altura 
-(em metros) ao quadrado. A fórmula é: IMC = Peso / (Altura x Altura). 
-Por exemplo, uma pessoa com 70 kg e 1,75 m de altura tem um IMC de 22,86, calculado assim: \(70\div (1,75\times 1,75)=22,86\). */
+/*O cálculo do IMC é feito dividindo o peso (em quilogramas) pela altura*/
